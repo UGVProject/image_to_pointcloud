05ca6d8482473c8ccdfa927bb0c7b69d9bb28d95
christlurker <hzhang032@e.ntu.edu.sg>
2016-08-22 12:26:51 +0800

    First commit

---
+++ src/image_pcl_node.cpp
@@ -0,0 +1,168 @@
+#include <algorithm>
+#include <chrono>
+#include <errno.h>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <cv_bridge/cv_bridge.h>
+#include <ros/ros.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <opencv2/core/core.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+// #include <sensor_msgs
+#include "../include/stereosgm.h"
+
+using namespace std;
+using namespace cv;
+
+class ImageGrabber {
+public:
+  ImageGrabber(stereosgm *pMATCHING) : mpMATCHING(pMATCHING) {}
+
+  void GrabImage(const sensor_msgs::ImageConstPtr &msg);
+
+  stereosgm *mpMATCHING;
+  std_msgs::Header head;
+  bool do_rectify;
+  cv::Mat M1l, M2l, M1r, M2r, Q;
+  int disp_size,scale;
+};
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "image_to_pcl_node");
+  ros::NodeHandle nh;
+  ros::NodeHandle n("~");
+  stereosgm SGM_Matching;
+
+  ImageGrabber igb(&SGM_Matching);
+  igb.scale = 2;
+  igb.disp_size = 128;
+  cv::Size fsize(640, 300);
+  std::string intrinsic_filename, extrinsic_filename;
+  intrinsic_filename = "/home/ugv/catkin_ws/src/image_to_pcl/intrinsics.yml";
+  extrinsic_filename = "/home/ugv/catkin_ws/src/image_to_pcl/extrinsics.yml";
+
+  if(n.getParam("scale", igb.scale))
+    ROS_INFO("Get scale: %d", igb.scale );
+  else
+    ROS_WARN("Using default scale: %d", igb.scale );
+
+  if(n.getParam("intrinsic_filename", intrinsic_filename))
+    ROS_INFO("Get intrinsic_filename" );
+  else
+    ROS_WARN("Using default scale" );
+
+
+  if(n.getParam("extrinsic_filename", extrinsic_filename))
+    ROS_INFO("Get extrinsic_filename" );
+  else
+    ROS_WARN("Using default scale" );
+
+  if(n.getParam("disparity_size", igb.disp_size))
+    ROS_INFO("Get disparity size: %d", igb.disp_size );
+  else
+    ROS_WARN("Using default size: %d", igb.disp_size);
+
+
+  // ros::start();
+
+  if (1) {
+    igb.do_rectify = true;
+
+    // Load settings related to stereo calibration
+    cv::FileStorage intrinsic_settings(intrinsic_filename, cv::FileStorage::READ);
+    if (!intrinsic_settings.isOpened()) {
+      cerr << "ERROR: Wrong path to settings of intrinsic_settings" << endl;
+      return -1;
+    }
+
+    cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r, R, T;
+    intrinsic_settings["M1"] >> K_l; K_l /= igb.scale;
+    intrinsic_settings["M2"] >> K_r; K_r /= igb.scale;
+
+    intrinsic_settings["D1"] >> D_l;
+    intrinsic_settings["D2"] >> D_r;
+
+    cv::FileStorage extrinsic_settings(extrinsic_filename, cv::FileStorage::READ);
+    if (!extrinsic_settings.isOpened()) {
+      cerr << "ERROR: Wrong path to settings of extrinsic_settings" << endl;
+      return -1;
+    }
+    extrinsic_settings["R"] >> R;
+    extrinsic_settings["T"] >> T;
+
+    if (K_l.empty() || K_r.empty() || R.empty() || T.empty() || D_l.empty() || D_r.empty() ) {
+      cerr << "ERROR: Calibration parameters to rectify stereo are missing!"
+           << endl;
+      return -1;
+    }
+    cv::Rect roi1, roi2;
+
+    cv::stereoRectify( K_l, D_l, K_r, D_r, fsize, R, T, R_l, R_r, P_l, P_r, igb.Q, cv::CALIB_ZERO_DISPARITY, -1, fsize, &roi1, &roi2 );
+    cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l, fsize,CV_16SC2,igb.M1l,igb.M2l);
+    cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r, fsize,CV_16SC2,igb.M1r,igb.M2r);
+  }
+
+  // ros::NodeHandle nodeHandler;
+  ros::Subscriber sub = nh.subscribe("/wide/image_raw", 1,
+                                              &ImageGrabber::GrabImage, &igb);
+
+
+  // ros::init(argc, argv, "publish_point_cloud");
+  ros::Publisher point_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB> > ("/camera/points", 1);
+
+
+
+  // ros::spin();
+  ros::Rate loop_rate(30);
+  // ros::Rate loop_rate(100);
+  while (nh.ok())
+  {
+    if(SGM_Matching.basic_cloud_ptr != NULL){
+      // SGM_Matching.basic_cloud_ptr->header.stamp = ros::Time::now().toNSec();
+      SGM_Matching.basic_cloud_ptr->header.stamp = igb.head.stamp.toNSec()/1000ull;
+      SGM_Matching.basic_cloud_ptr->header.frame_id = "map";
+      // pcl_conversions::toPCL(igb.head, SGM_Matching.basic_cloud_ptr->header);
+      // pcl_conversions::toPCL(ros::Time::now(), SGM_Matching.basic_cloud_ptr->header.stamp);
+      point_pub.publish(SGM_Matching.basic_cloud_ptr);
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+
+  ros::shutdown();
+
+  return 0;
+}
+
+void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr &msg) {
+  // Copy the ros image message to cv::Mat.
+  cv_bridge::CvImagePtr cv_ptr;
+  head = msg->header;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+  } catch (cv_bridge::Exception &e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+  cv::Mat image_full, image_scale, imLeft1, imRight1;
+  image_full = cv_ptr->image;
+  cv::Size rzSize(2560/scale, 1024/scale);
+  cv::resize(image_full, image_scale, rzSize);
+  imLeft1 = image_scale(Range(75, 375), Range(0/scale, 1280/scale));
+  imRight1 = image_scale(Range(75, 375), Range(1280/scale, 2560/scale));
+
+  if (1) {
+    Mat imLeftRec, imRightRec;
+    remap(imLeft1, imLeftRec, M1l, M2l, cv::INTER_LINEAR);
+    remap(imRight1, imRightRec, M1r, M2r, cv::INTER_LINEAR);
+    mpMATCHING->StereoMatching(imLeftRec, imRightRec, Q, cv_ptr->header.stamp.toSec());
+  } else {
+    mpMATCHING->StereoMatching(imLeft1, imRight1, Q, cv_ptr->header.stamp.toSec());
+  }
+}
---
+++ src/stereosgm.cpp
@@ -0,0 +1,163 @@
+// stereosgm.cpp
+// Created by Zhang Handuo on 08/04/16.
+// Modified by Zhang Handuo on 22/04/16.
+
+#include "../include/stereosgm.h"
+stereosgm::stereosgm( ):
+        mWidth(640),
+        mHeight(300),
+        disp_size(128)
+{}
+
+stereosgm::~stereosgm() {}
+
+void stereosgm::StereoMatching(const cv::Mat &imLeft, const cv::Mat &imRight, cv::Mat Q, const double &timestamp)
+{
+  int bits = 8;
+  switch (imLeft.type()) {
+    case CV_8UC1: bits = 8; left = imLeft; right = imRight; break;
+    case CV_16UC1: bits = 16; left = imLeft; right = imRight; break;
+    default:
+    std::cerr << "invalid input image color format" << imLeft.type() << " So now converting to gray picture: " << std::endl;
+    cvtColor(imLeft, left, CV_RGB2GRAY);
+    cvtColor(imRight, right, CV_RGB2GRAY);
+  }
+  struct timeval start, end;
+  long mtime, seconds, useconds;
+  sgm::StereoSGM ssgm(imLeft.cols, imLeft.rows, disp_size, bits, 8, sgm::EXECUTE_INOUT_HOST2HOST);
+	cv::Mat output(cv::Size(imLeft.cols, imLeft.rows), CV_8UC1);
+
+	gettimeofday(&start, NULL);
+	ssgm.execute(left.data, right.data, (void**)&output.data);
+	gettimeofday(&end, NULL);
+
+	seconds  = end.tv_sec  - start.tv_sec;
+  useconds = end.tv_usec - start.tv_usec;
+  mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+  std::cout << "Elapsed time: " << mtime << "milliseconds!"  << std::endl;
+
+  // cvtColor(left, left_color, CV_GRAY2RGB);
+  //3D point cloud caluculation
+  cv::Mat cloud(output.size(),CV_32FC3);
+  reprojectImageTo3D(output, cloud, Q, true);
+// std::cout << "image type: " << type2str(left_color.type() ).c_str() << std::endl;
+
+  // std::cout << "Genarating example point clouds.\n\n";
+  // cv::imshow("image", output * 256 / disp_size);
+  // cv::waitKey(1);
+
+  // return cloud;
+  visualizer(cloud);
+
+}
+
+void stereosgm::visualizer(cv::Mat &cloud){
+  // pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+  // pcl::PointCloud<pcl::PointXYZRGB>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
+  basic_cloud_ptr = pcl::PointCloud<pcl::PointXYZRGB>::Ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
+  uint8_t r;
+  // uint8_t r, g, b;
+  for (int i=0; i<cloud.rows; i++){
+    uchar* rgb_ptr = left.ptr<uchar>(i);
+    for (int j=0; j<cloud.cols; j++){
+      r = rgb_ptr[j];
+
+      pcl::PointXYZRGB basic_point;
+      cv::Vec3f cloudpoint = cloud.at<cv::Vec3f>(i, j);
+      if (std::isfinite(cloudpoint[0]) && std::isfinite(cloudpoint[1]) && std::isfinite(cloudpoint[2]) ){
+        basic_point.x = cloudpoint[0]/1000;
+        basic_point.y = cloudpoint[1]/1000;
+        basic_point.z = cloudpoint[2]/1000;
+      //   uint32_t rgb = (static_cast<uint8_t>(r) << 16|
+      // static_cast<uint8_t>(g) << 8 | static_cast<uint8_t>(b));
+        uint32_t rgb = (static_cast<uint32_t>(r) << 16|
+      static_cast<uint32_t>(r) << 8 | static_cast<uint32_t>(r));
+        basic_point.rgb = *reinterpret_cast<float*>(&rgb);
+        basic_cloud_ptr->points.push_back(basic_point);
+        //std::cout<< " ;"<<cloudpoint;//<<std::endl;
+      }
+    }
+  }
+
+  //std::cout<<points.size ()<<std::endl;
+  basic_cloud_ptr->width = (int) basic_cloud_ptr->points.size ();
+  basic_cloud_ptr->height = 1;
+  // basic_cloud_ptr->header.frame_id = "camera";
+  // basic_cloud_ptr->
+
+  // boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  // viewer = simpleVis(basic_cloud_ptr);
+  // // viewer->spinOnce (100);
+  // // cv::imshow("image", output);
+  // // cv::waitKey(1);
+  // //
+  // while (!viewer->wasStopped ())
+  // {
+  //   viewer->spinOnce (100);
+  // //
+  //   boost::this_thread::sleep (boost::posix_time::microseconds (100));
+  // //
+  // //   // boost::mutex::scoped_lock updateLock(updateModelMutex);
+  // //   // if(update)
+  // //   // {
+  // //   //     if(!viewer->updatePointCloud(cloud, "sample cloud"))
+  // //   //       viewer->addPointCloud(cloud, colorHandler, "sample cloud");
+  // //   //     update = false;
+  // //   // }
+  // //   // updateLock.unlock();
+  // }
+
+}
+
+std::string stereosgm::type2str(int type) {
+  std::string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
+bool stereosgm::compare_mean(const cv::Point3i &a, const cv::Point3i &b) {
+  return a.x > b.x;
+}
+
+bool stereosgm::compare_row(const cv::Point3i &a, const cv::Point3i &b) {
+  return a.y > b.y;
+}
+
+bool stereosgm::compare_col(const cv::Point3i &a, const cv::Point3i &b) {
+  return a.z > b.z;
+}
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> stereosgm::simpleVis (pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud)
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  // pcl::visualization::Camera camera_;
+  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
+  viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+  viewer->setCameraPosition(0,0,0,0,0,1,0,-1,0);
+  // viewer->updateCamera();
+  return (viewer);
+}
